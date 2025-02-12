229363ebd3481f707b8fc859e6fb120e9436cd76
idwebtiga <idwebtiga@gmail.com>
2025-02-12 14:56:08 +0700

    set express public

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('v1.0.1');
+  res.send('v1.0.2');
 });
 
 require('./routes/messages.routes')(app);
