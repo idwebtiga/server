6ce88e38b1c4ab0e08634d28c5f61df52b166bcf
idwebtiga <idwebtiga@gmail.com>
2025-02-12 15:11:24 +0700

    set public path

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 // Import packages
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 // Middlewares
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.send('v1.0.2');
+  res.send('OK')
 });
 
 require('./routes/messages.routes')(app);
